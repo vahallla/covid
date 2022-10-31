b4627a82fd04d1bb8e4b3bb1e4172196af88d2a0
vahallla <89891803+vahallla@users.noreply.github.com>
2022-10-31 16:48:29 +0900

    1031-kyw covid support-수정중

---
+++ app/src/main/java/com/example/covid/map_java.java
@@ -0,0 +1,17 @@
+package com.example.covid;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class map_java extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedIntenceStae) {
+        super.onCreate(savedIntenceStae);
+        setContentView(R.layout.map);
+    }
+
+}
