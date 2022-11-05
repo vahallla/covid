4a0fcaf9604bd3e47b85e98bd871d9b4099de111
vahallla <89891803+vahallla@users.noreply.github.com>
2022-11-05 21:00:25 +0900

    1105-대처방안 뼈대(세준,민수 담당)

--- app/src/main/java/com/example/covid/covid_java.java
+++ app/src/main/java/com/example/covid/covid_java.java
@@ -1,18 +1,40 @@
 package com.example.covid;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class covid_java extends AppCompatActivity{
+public class covid_java extends AppCompatActivity implements View.OnClickListener {
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedIntenceStae) {
         super.onCreate(savedIntenceStae);
         setContentView(R.layout.covid);
+
+        Button btn7 = (Button) findViewById(R.id.button);
+        btn7.setOnClickListener(this);
+        Button btn8 = (Button) findViewById(R.id.button2);
+        btn8.setOnClickListener(this);
+
+
     }
 
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.button) {
+            Intent intent = new Intent(covid_java.this, cvd.class);
+            startActivity(intent);
+        } else if (view.getId() == R.id.button2) {
+            Intent intent = new Intent(covid_java.this, cvd.class);
+            startActivity(intent);
+        }
+    }
 }
+
+
