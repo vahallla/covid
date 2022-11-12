c2bec4290ca2bfc9a58c7fba0bf91d32ee6bbd0a
vahallla <89891803+vahallla@users.noreply.github.com>
2022-11-12 21:24:30 +0900

    코로나 일일확진/재원 위중증/신규 입원/일일 사망 추가 메인/생필품 신청에 추가

--- app/src/main/java/com/example/covid/MainActivity.java
+++ app/src/main/java/com/example/covid/MainActivity.java
@@ -2,21 +2,36 @@ package com.example.covid;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-
+    String[] nums={"","","","",""};
+    String numsa="";
+    TextView textView1,textView2,textView3,textView4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-
+        textView1 = findViewById(R.id.supview1);
+        textView2 = findViewById(R.id.supview2);
+        textView3 = findViewById(R.id.supview3);
+        textView4 = findViewById(R.id.supview4);
+        final Bundle bundle = new Bundle();
         Button btn1 = findViewById(R.id.button_move1);//버튼 1,2,3선언
         btn1.setOnClickListener(this);
         Button btn2 = findViewById(R.id.button_move2);
@@ -24,6 +39,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button btn3 = findViewById(R.id.button_move3);
         btn3.setOnClickListener(this);
 
+        new Thread(){
+            @Override
+            public void run() {
+                Document doc;
+                try {
+                    doc = Jsoup.connect("https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&qvt=0&query=%EC%BD%94%EB%A1%9C%EB%82%9819").get();
+
+                    for(int i = 1; i < 5; i++){
+                        Element contents = doc.select(".info_0"+i).first();                 //복권 번호 6개 가져오기
+                        nums[i] = " "+contents.text();
+                        bundle.putString("numbers"+i, nums[i]);
+                        Message msg = handler.obtainMessage();
+                        msg.setData(bundle);
+                        handler.sendMessage(msg);
+
+
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
 
 
     }
@@ -44,7 +82,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         }
-
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            Bundle bundle = msg.getData();
+            textView1.setText(bundle.getString("numbers1"));
+            textView2.setText(bundle.getString("numbers2"));
+            textView3.setText(bundle.getString("numbers3"));
+            textView4.setText(bundle.getString("numbers4"));
+        }
+    };
 
 
 }
--- app/src/main/java/com/example/covid/support_java.java
+++ app/src/main/java/com/example/covid/support_java.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.view.WindowManager;
 import android.webkit.WebChromeClient;
@@ -11,29 +13,72 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+
 public class support_java extends AppCompatActivity {
+
 
     Button btn4;
     Button btn5;
     Button btn6;
+    String[] nums={"","","","",""};
+    String numsa="";
+    TextView textView1,textView2,textView3,textView4;
+
 
     WebView webview1;
     //private String url = "https://covid19m.kdca.go.kr/KP/epq/35700092A";
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedIntenceStae) {
         super.onCreate(savedIntenceStae);
         setContentView(R.layout.support);
 
+        textView1 = findViewById(R.id.supview1);
+        textView2 = findViewById(R.id.supview2);
+        textView3 = findViewById(R.id.supview3);
+        textView4 = findViewById(R.id.supview4);
+
+        final Bundle bundle = new Bundle();
+
         btn4 = findViewById(R.id.imageView4);
         btn5 = findViewById(R.id.imageView5);
         btn6 = findViewById(R.id.covid_document);
 
+        new Thread(){
+            @Override
+            public void run() {
+                Document doc;
+                try {
+                    doc = Jsoup.connect("https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&qvt=0&query=%EC%BD%94%EB%A1%9C%EB%82%9819").get();
+
+                    for(int i = 1; i < 5; i++){
+                        Element contents = doc.select(".info_0"+i).first();                 //복권 번호 6개 가져오기
+                        nums[i] = " "+contents.text();
+                        bundle.putString("numbers"+i, nums[i]);
+                        Message msg = handler.obtainMessage();
+                        msg.setData(bundle);
+                        handler.sendMessage(msg);
+
+
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
 
 
         btn4.setOnClickListener(new View.OnClickListener() {
@@ -41,10 +86,8 @@ public class support_java extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.iksan.go.kr/index.iksan?menuCd=DOM_000002018001002000"));
                 startActivity(intent);
-
             }
         });
-
         btn5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -52,7 +95,6 @@ public class support_java extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
         btn6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -61,6 +103,15 @@ public class support_java extends AppCompatActivity {
             }
         });
     }
-
-
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            Bundle bundle = msg.getData();
+            textView1.setText(bundle.getString("numbers1"));
+            textView2.setText(bundle.getString("numbers2"));
+            textView3.setText(bundle.getString("numbers3"));
+            textView4.setText(bundle.getString("numbers4"));
+        }
+    };
 }
+
